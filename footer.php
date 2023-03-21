b02020e0da43bf5a024ec7e414a0b914fde66aed
vishal1145 <vishal.kumar1145@gmail.com>
2023-03-21 10:48:08 +0530

    footer and csStudent info changes

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 			<div class="col-md-2 ">
 				<h3 class="quick">Connect with Us</h3>
 				<div class="links-footerDiv"> <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D">Twitter</a></div>
-				<div class="links-footerDiv"> <a href="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C589460569849%7Cb%7Cfacebook%20%27%7C&placement=&creative=589460569849&keyword=facebook%20%27&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-327195741349%26loc_physical_ms%3D1007826%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAjw5dqgBhBNEiwA7PryaGELthdUgOP4cqyQGkQHrD0ry6oRBXsP_IeN2CaytodHGLZ-tLt87BoCy4cQAvD_BwE">Facebook</a></div>
+				<div class="links-footerDiv"> <a href="https://www.facebook.com/people/SIR-C-R-REDDY-Polytechnic/100063868821052/">Facebook</a></div>
 				<div class="links-footerDiv"> <a href="https://www.instagram.com/">Instagram</a>
 				</div>
 
