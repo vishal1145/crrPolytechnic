485e2c7ec9a605af1a639c2f30a7fd345b2e6a35
vishal1145 <vishal.kumar1145@gmail.com>
2023-04-21 16:38:48 +0530

    changes

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("mysql-121079-0.cloudclusters.net", "admin", "eO0P7rYz", "crrPolytechnic", "19848") or die("connection failed");
+$conn = mysqli_connect("mysql-122600-0.cloudclusters.net", "admin", "7NMrKaD4", "crrPolytechnic", "10005") or die("connection failed");
 $base_url = "http://127.0.0.1:8000";
 $image_url  = "http://34.16.138.135:8000/storage";
 ?>
